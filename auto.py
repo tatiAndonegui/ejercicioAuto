3fdc45dd681b82ce7ac38652be6f047f3dd34c43
tatiAndonegui <115169557+tatiAndonegui@users.noreply.github.com>
2024-05-08 00:39:57 +0000

    prueba

---
+++ auto.py
@@ -0,0 +1,10 @@
+class automovil:
+
+
+def __init__(self, color, marca,aceleracion,velocidad):
+    self.ruedas = 4
+    self.color = color
+    self.marca = marca
+    self.aceleracion = aceleracion
+    self.velocidad = velocidad  
+
